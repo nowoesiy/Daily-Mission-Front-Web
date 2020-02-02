6481977ffbe4c49f815fff50a7ba8f9db6622830
nowoesiy <nowoesiy@gmail.com>
2020-02-02 15:11:31 +0900

    Feat : Add Status Components

---
+++ src/components/Status/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+class Status extends React.Component {
+
+  render(){
+    return (
+        <div className="Container-status">
+            <h1>이수백 님의 미션</h1>
+        </div>
+    );
+  }
+}
+
+export default Status;
