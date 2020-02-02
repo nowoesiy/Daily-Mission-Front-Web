2a8aa62a286f98e049b6334c6e311bd2cebba8f9
nowoesiy <nowoesiy@gmail.com>
2020-02-02 15:11:02 +0900

    Feat : Add Aside Components

---
+++ src/components/Aside/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+class Aside extends React.Component {
+
+  render(){
+    return (
+        <div className="App-aside">
+        </div>
+    );
+  }
+}
+
+export default Aside;
