291f54b90bf4478bd277b9b38ff2272a6033b3e7
nhseo <tjdnjs3664@gmail.com>
2020-02-15 23:28:35 +0900

    Feat: Login Form change

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,37 +2,53 @@ import React from 'react';
 import './index.scss';
 import { GOOGLE_AUTH_URL } from '../../constants';
 import googlelogo from '../../static/img/google-logo.png';
+import kakaologo from '../../static/img/kakao--logo.png';
+import naverlogo from '../../static/img/naver-logo.png';
+
 class Login extends React.Component {
   render() {
     return (
       <div className="login-modal">
-        <div className="login-modal__title">소셜계정 로그인</div>
+        <div className="login-modal__title">로그인</div>
         <a href={GOOGLE_AUTH_URL}>
-          <button className="login-modal__button" type="button">
+          <button
+            className="login-modal__button login-modal__button--google"
+            type="button"
+          >
             <img
               style={{ width: '25px', height: '25px' }}
               src={googlelogo}
               alt="google"
             ></img>
-            <span>구글로 시작하기</span>
+            <span> 구글로 시작하기</span>
           </button>
         </a>
         <a href={GOOGLE_AUTH_URL}>
           <button
-            className="login-modal__button"
+            className="login-modal__button login-modal__button--kakao"
             type="button"
             href={GOOGLE_AUTH_URL}
           >
-            카카오로 시작하기
+            <img
+              style={{ width: '25px', height: '25px' }}
+              src={kakaologo}
+              alt="kakao"
+            ></img>
+            <span> 카카오로 시작하기</span>
           </button>
         </a>
         <a href={GOOGLE_AUTH_URL}>
           <button
-            className="login-modal__button"
+            className="login-modal__button login-modal__button--naver"
             type="button"
             href={GOOGLE_AUTH_URL}
           >
-            네이버로 시작하기
+            <img
+              style={{ width: '25px', height: '25px' }}
+              src={naverlogo}
+              alt="naver"
+            ></img>
+            <span> 네이버로 시작하기</span>
           </button>
         </a>
       </div>
