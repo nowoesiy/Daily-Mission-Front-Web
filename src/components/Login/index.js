98a213f7620f70273e213571c396e022e2cd905c
nhseo <tjdnjs3664@gmail.com>
2020-05-29 09:49:16 +0900

    Fix: refactoring...

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -10,59 +10,57 @@ import googlelogo from '../../static/img/google-logo.png';
 import githublogo from '../../static/img/github--logo.png';
 import naverlogo from '../../static/img/naver-logo.png';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div className="login-modal">
-        <img
-          className="login-modal__logo"
-          src={logo}
-          alt="daily-mission-logo"
-        ></img>
-        <div className="login-modal__title">로그인</div>
-        <a href={GOOGLE_AUTH_URL}>
-          <button
-            className="login-modal__button login-modal__button--google"
-            type="button"
-          >
-            <img
-              className="login-modal__button-image"
-              src={googlelogo}
-              alt="google"
-            ></img>
-            <span> 구글로 시작하기</span>
-          </button>
-        </a>
-        <a href={GITHUB_AUTH_URL}>
-          <button
-            className="login-modal__button login-modal__button--kakao"
-            type="button"
-            href={GITHUB_AUTH_URL}
-          >
-            <img
-              className="login-modal__button-image"
-              src={githublogo}
-              alt="github"
-            ></img>
-            <span> 깃허브로 시작하기</span>
-          </button>
-        </a>
-        <a href={NAVER_AUTH_URL}>
-          <button
-            className="login-modal__button login-modal__button--naver"
-            type="button"
-          >
-            <img
-              className="login-modal__button-image"
-              src={naverlogo}
-              alt="naver"
-            ></img>
-            <span> 네이버로 시작하기</span>
-          </button>
-        </a>
-      </div>
-    );
-  }
-}
+const Login = () => {
+  return (
+    <div className="login-modal">
+      <img
+        className="login-modal__logo"
+        src={logo}
+        alt="daily-mission-logo"
+      ></img>
+      <div className="login-modal__title">로그인</div>
+      <a href={GOOGLE_AUTH_URL}>
+        <button
+          className="login-modal__button login-modal__button--google"
+          type="button"
+        >
+          <img
+            className="login-modal__button-image"
+            src={googlelogo}
+            alt="google"
+          ></img>
+          <span> 구글로 시작하기</span>
+        </button>
+      </a>
+      <a href={GITHUB_AUTH_URL}>
+        <button
+          className="login-modal__button login-modal__button--kakao"
+          type="button"
+          href={GITHUB_AUTH_URL}
+        >
+          <img
+            className="login-modal__button-image"
+            src={githublogo}
+            alt="github"
+          ></img>
+          <span> 깃허브로 시작하기</span>
+        </button>
+      </a>
+      <a href={NAVER_AUTH_URL}>
+        <button
+          className="login-modal__button login-modal__button--naver"
+          type="button"
+        >
+          <img
+            className="login-modal__button-image"
+            src={naverlogo}
+            alt="naver"
+          ></img>
+          <span> 네이버로 시작하기</span>
+        </button>
+      </a>
+    </div>
+  );
+};
 
 export default Login;
